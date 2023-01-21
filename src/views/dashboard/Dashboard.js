1379557cae5c59979f7a0d249673d2eb2a385f2c
Jacob Roeters van Lennep <jacob@intelligentfridges.com>
2023-01-21 18:08:27 +0100

    add currentLocation functionality, tryout with multiple cities

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,14 +1,50 @@
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import WeatherBlock from './components/WeatherBlock';
+import { useGeolocated } from 'react-geolocated';
+import { useEffect } from 'react';
 
 
 function Dashboard() {
+
+    const locations = [
+        {name: 'Amsterdam', lon: 4.9041, lat: 52.3676},
+        {name: 'Berlin', lon: 13.4050, lat: 52.5200},
+        {name: 'London', lon: 0.1276, lat:51.5072},
+    ];
+
+    const currentLocation = useGeolocated({
+        positionOptions: {
+            enableHighAccuracy: false,
+        },
+        userDecisionTimeout: 5000,
+    });
+
+    useEffect(() => {
+        console.log(currentLocation);
+    }, [currentLocation]);
+
+
     return (
         <Container>
-            <Grid container>
-                <Grid item>
-                    <WeatherBlock location={{lon: 4.9041, lat: 52.3676, name: 'Amsterdam'}}/>
-                </Grid>
+            <Grid container spacing={2}>
+                {currentLocation.isGeolocationEnabled &&
+                    <Grid item>
+                        <WeatherBlock
+                            location={
+                                {
+                                    lon: currentLocation.coords?.longitude,
+                                    lat: currentLocation.coords?.latitude,
+                                    name: 'Current Location'
+                                }
+                            }
+                        />
+                    </Grid>
+                }
+                {locations.map(location =>
+                        <Grid item>
+                            <WeatherBlock location={location}/>
+                        </Grid>
+                    )}
             </Grid>
         </Container>
     )
